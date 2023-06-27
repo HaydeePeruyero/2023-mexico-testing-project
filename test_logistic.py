51d7dd4dd2153001ac8dea2a6343c396c8604f9a
ASPP Student <student@localhost>
2023-06-27 14:21:04 -0600

    using fixture

--- test_logistic.py
+++ test_logistic.py
@@ -2,6 +2,8 @@ import pytest
 from logistic import f, iterate_f, random_generator
 from numpy.testing import assert_allclose
 from math import isclose
+import numpy as np
+
 
 @pytest.mark.xfail
 @pytest.mark.parametrize("a", [1,2,3])
@@ -17,11 +19,32 @@ def test_logistic(x,r, expected):
 	assert isclose(results, expected)
 	
 
-def test_converge():
-	SEED = 41
-	x0 = random_generator(SEED)
+
+
+@pytest.fixture
+def random_state():
+	SEED = np.random.randint(0, 2**31)
+	print(f'Using sedd {SEED}')
+	rs = np.random.RandomState(SEED)
+	return rs
+
+
+def test_converge(random_state):
+	#SEED = 41
+	#x0 = random_generator(SEED)
+	x0 = random_state.rand()
 	result = iterate_f(100, x0, 1.5)
-	assert isclose(result[99], 1/3)
+	assert isclose(result[-1], 1/3)
 	
+	
+def test_con():
+	seed = 1213
+	random_state = np.random.RandomState(seed)
+	x0 = random_state.rand()
+	xs = iterate_f(100,x0,1.5)
+	expected=1/3
+	assert isclose(xs[-1],expected) 
+	
+
 	
 
