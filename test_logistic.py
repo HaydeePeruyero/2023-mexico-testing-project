eeb3d000f825e77c036cfbcb5662ff6cb7360991
ASPP Student <student@localhost>
2023-06-27 12:18:33 -0600

    try xfail

--- test_logistic.py
+++ test_logistic.py
@@ -1,13 +1,20 @@
 import pytest
-from logistic import logistic_map, iterate_f
+from logistic import f, iterate_f
 from numpy.testing import assert_allclose
 from math import isclose
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("a", [1,2,3])
+@pytest.mark.parametrize("b", [2,5,3])
+def test_sums():
+	assert a + b < a
 
 @pytest.mark.parametrize(
 	"x, r, expected", 
 	[(0.1,2.2, 0.198),(0.2,3.4, 0.544),(0.75,1.7,0.31875)])
-
-
 def test_logistic(x,r, expected):
-	results = logistic_map(x,r)
+	results = f(x,r)
 	assert isclose(results, expected)
+	
+	
+
